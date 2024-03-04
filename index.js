9e269a748275fb9d57c04da76c22d8d2eb6799a2
Gremogt <158091522+Gremogt@users.noreply.github.com>
2024-03-04 11:58:03 +0530

    Update index.js

--- index.js
+++ index.js
@@ -8,4 +8,11 @@ bot.on("error", (err) => {
   console.error(err); // or your preferred logger
 });
 
+  // Set the bot's status
+  bot.editStatus("idle", {
+    name: "NixonNodes.com",  // Replace with the desired status text
+    type: 3,  // 0 for playing, 1 for streaming, 2 for listening, 3 for watching
+  });
+});
+
 bot.connect(); // Get the bot to connect to Discord
